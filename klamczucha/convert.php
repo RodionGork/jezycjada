f54416b06ddb64b6a73f7d399d13658bdf27a8c3
Rodion <rodiongork@github.com>
2024-02-04 10:20:39 +0300

    Part 7, some conversion improvements

--- klamczucha/convert.php
+++ klamczucha/convert.php
@@ -3,17 +3,33 @@
 require 'MarkdownInterface.php';
 require 'Markdown.php';
 
-if (count($argv) < 3) {
-    echo "Usage: php convert.php input.md output.html\n";
+if (count($argv) < 2) {
+    echo "Usage: php convert.php input.txt\n";
 }
 
 $mp = new \Michelf\Markdown();
 
-$output = file_get_contents($argv[2]);
-$output = substr($output, 0, strpos($output, '<body>') + 6) . "\n";
-
 $text = file_get_contents($argv[1]);
+
+$title = [];
+preg_match('/\[title\]\:.*?\(([^\)]+)\)/', $text, $title);
 
 $text = str_replace('--', '&ndash;', $text);
 
-file_put_contents($argv[2], $output . $mp->transform($text) . '</body></html>');
+$body = $mp->transform($text);
+
+$output = "<!doctype html>
+<html>
+<head>
+  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/>
+  <meta charset=\"utf-8\"/>
+  <title>Кламчуха (Лгунья) - М.Мусерович - часть 1 - {$title[1]}</title>
+  <link rel=\"stylesheet\" href=\"./common.css\"/>
+</head>
+<body>
+$body
+</body>
+</html>
+";
+
+file_put_contents(str_replace('.txt', '.html', $argv[1]), $output);
